b384cdb812cbc0f5479823992fe78bcd242c578a
Wolfgang <ludwig.ludwig.anderson@gmail.com>
2023-03-30 10:18:16 +0200

    results corrections

--- app_install_pacman.sh
+++ app_install_pacman.sh
@@ -36,6 +36,7 @@ PROGRAMS=(
   "virtualbox"
   "virtualbox-guest-utils"
   )
+
 # Counter for failed installations
 FAILED=0
 
@@ -43,11 +44,11 @@ FAILED=0
 for PROGRAM in "${PROGRAMS[@]}"; do
   # Check if program is already installed
   if command -v "$PROGRAM" >/dev/null 2>&1; then
-    echo "$PROGRAM is already installed"
+    echo -e "\e[32m$PROGRAM is already installed\e[0m"
   else
     # Install program
     if sudo pacman -S --noconfirm "$PROGRAM"; then
-      echo "$PROGRAM successfully installed"
+      echo -e "\e[32m$PROGRAM successfully installed\e[0m"
     else
       echo -e "\e[31mFailed to install $PROGRAM\e[0m"
       ((FAILED++))
